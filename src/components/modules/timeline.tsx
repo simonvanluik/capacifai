f688f1ebcff1b5de1800dedf0d2a86b29c3d1897
simonvanluik <simonvanluik@hotmail.com>
2025-01-07 21:54:35 +0100

    removed !

--- src/components/modules/timeline.tsx
+++ src/components/modules/timeline.tsx
@@ -9,9 +9,9 @@ export default function TasksPage() {
     <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
       <div className="flex items-center justify-between space-y-2">
         <div>
-          <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+          <h2 className="text-2xl font-bold tracking-tight">Welcome back</h2>
           <p className="text-muted-foreground">
-            Here&apos;s a list of your tasks for this month!
+            Here&apos;s a list of your tasks for this month
           </p>
         </div>
       </div>
